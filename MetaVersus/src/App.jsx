d6ab349fb95d423002dbe6428452e4bc64b9d707
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-18 19:20:08 +0530

    Tailwind setup with vite

---
+++ MetaVersus/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="App bg-green-500 text-white">
+			<h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+		</div>
+	);
+}
+
+export default App;
