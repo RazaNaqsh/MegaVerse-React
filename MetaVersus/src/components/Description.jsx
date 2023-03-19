7ec6e5d68104e23b04d364f0b99d7a7bbf60576b
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-20 00:22:54 +0530

    add viewport and whileinView prop to desc

--- MetaVersus/src/components/Description.jsx
+++ MetaVersus/src/components/Description.jsx
@@ -1,7 +1,21 @@
+import { motion } from "framer-motion";
+
 const Description = () => {
 	return (
-		<section className="w-[60vw] m-auto mt-36 text-center">
-			<h1 className="">|About MetaVersus</h1>
+		<motion.section
+			className="w-[60vw] m-auto mt-36 text-center"
+			initial={{ opacity: 0, x: -200 }}
+			whileInView={{ opacity: 1, x: 0 }}
+			viewport={{ once: false }}
+			transition={{
+				type: "spring",
+				mass: 1,
+				damping: 8,
+				delay: 1,
+				duration: 0.4,
+			}}
+		>
+			<h1 className="font-sans font-light">|About MetaVersus</h1>
 			<p className="mt-4 text-4xl leading-tight font-sans  font-thin">
 				Metaverse is a new thing in the future, where you can enjoy the virtual
 				world by feeling like it's really real, you can feel what you feel in
@@ -10,7 +24,7 @@ const Description = () => {
 				metaverse world you want, turn your dreams into reality. Let's explore
 				the madness of the metaverse by scrolling down
 			</p>
-		</section>
+		</motion.section>
 	);
 };
 
