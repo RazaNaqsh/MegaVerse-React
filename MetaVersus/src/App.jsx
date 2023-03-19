45a30a75c636e478b91b7026da08ead1a6a98bf7
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-19 23:51:38 +0530

    Description section text added

--- MetaVersus/src/App.jsx
+++ MetaVersus/src/App.jsx
@@ -1,11 +1,13 @@
+import Description from "./components/Description";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
 	return (
-		<div className="bg-[#1A232E] px-28">
+		<div className="bg-[#1A232E] px-28 text-white">
 			<Navbar />
 			<Hero />
+			<Description />
 		</div>
 	);
 }
---
+++ MetaVersus/src/components/Description.jsx
@@ -0,0 +1,17 @@
+const Description = () => {
+	return (
+		<section className="w-[60vw] m-auto mt-36 text-center">
+			<h1 className="">|About MetaVersus</h1>
+			<p className="mt-4 text-4xl leading-tight font-sans  font-thin">
+				Metaverse is a new thing in the future, where you can enjoy the virtual
+				world by feeling like it's really real, you can feel what you feel in
+				this metaverse world, because this is really the madness of the
+				metaverse of today, using only VR devices you can easily explore the
+				metaverse world you want, turn your dreams into reality. Let's explore
+				the madness of the metaverse by scrolling down
+			</p>
+		</section>
+	);
+};
+
+export default Description;
